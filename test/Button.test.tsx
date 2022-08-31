ba5f1d6f6467d56864ffd6a1a6594929e8275e00
korytovaf <kaf1@mail.ru>
2022-08-31 17:10:13 +0300

    feat: add button component

---
+++ test/Button.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Button } from '../src';
+
+describe('Button', () => {
+  describe('Должно правильно рендериться', () => {
+    test('default', () => {
+      render(
+        <Button
+          onClick={() => console.log('Меня кликнули')}
+          isDisabled={false}
+          variant="primary"
+        >
+          Я кнопка
+        </Button>
+      );
+      const button = screen.getByText('Я кнопка');
+      expect(button.classList.contains('button-primary')).toBeTruthy();
+    });
+    // test("success", () => {})
+    // test("disabled", () => {})
+  });
+
+  // test("Кнопка кликабельна", () => {})
+  // test("Кнопка не кликабельна если состояние disabled", () => {})
+});
